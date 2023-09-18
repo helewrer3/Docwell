5218e47955856b94d8cd3e3e7a18bb0616a3a44f
Garvit Kansal <helewrer3@Garvits-MacBook-Pro-2.local>
2023-09-18 19:34:52 +0530

    fuck it #10

--- development.local.sh
+++ development.local.sh
@@ -5,7 +5,7 @@ set -o nounset \
     -o verbose \
     -o xtrace
 
-DOCKER_COMPOSE_FILE="docker-compose.yml"
+DOCKER_COMPOSE_FILE="docker-compose.local.yml"
 docker-compose -f "$DOCKER_COMPOSE_FILE" --env-file .env.development.local down
 docker-compose -f "$DOCKER_COMPOSE_FILE" --env-file .env.development.local build
 docker-compose -f "$DOCKER_COMPOSE_FILE" --env-file .env.development.local up
